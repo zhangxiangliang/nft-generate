81802ba6f2adb728a27689b94b13021008c8c51a
zhangxiangliang <admin@pushme.top>
2022-04-06 02:21:22 +0800

    feat: setup panel components

---
+++ components/panel/GenerateAttributePanel.tsx
@@ -0,0 +1,83 @@
+// React
+import { FC } from "react";
+
+// Icon
+import {
+  TrashIcon,
+  ChevronUpIcon,
+  ChevronDownIcon,
+} from "@heroicons/react/solid";
+
+// Components
+import Button from "components/ui/Button";
+
+// Provider
+import {
+  GenerateAttribute,
+  initialGenerateAttribute,
+} from "provider/GenerateAttributeProvider";
+
+export interface GenerateAttributePanelProps {
+  isLast: boolean;
+  isFirst: boolean;
+
+  title?: string;
+  className?: string;
+  attribute?: GenerateAttribute;
+  onAttributeChange?: (
+    attribute: GenerateAttribute,
+    changed: Partial<GenerateAttribute>
+  ) => void;
+}
+
+export const GenerateAttributePanel: FC<GenerateAttributePanelProps> = ({
+  isLast = false,
+  isFirst = false,
+
+  attribute = { ...initialGenerateAttribute },
+  onAttributeChange = () => "",
+}) => {
+  return (
+    <section className="border-2 rounded-md border-black border-opacity-10">
+      <header className="border-b-2 border-black border-opacity-10 p-5 flex justify-between items-center">
+        <input
+          type="text"
+          value={attribute.name}
+          className="focus:outline-none w-1/2"
+          onChange={(event) =>
+            onAttributeChange(attribute, { name: event.target.value })
+          }
+        />
+
+        <section className="space-x-1">
+          {!isFirst && (
+            <Button
+              onClick={() =>
+                onAttributeChange(attribute, { sort: attribute.sort - 1 })
+              }
+            >
+              <ChevronUpIcon className="h-5 w-5 text-white" />
+            </Button>
+          )}
+          {!isLast && (
+            <Button
+              onClick={() =>
+                onAttributeChange(attribute, { sort: attribute.sort + 1 })
+              }
+            >
+              <ChevronDownIcon className="h-5 w-5 text-white" />
+            </Button>
+          )}
+          <Button onClick={() => onAttributeChange(attribute, {})}>
+            <TrashIcon className="h-5 w-5 text-white" />
+          </Button>
+        </section>
+      </header>
+      <main className="p-5">
+        {attribute.images.length === 0 && <p>暂无任何图片</p>}
+      </main>
+    </section>
+  );
+};
+
+export default GenerateAttributePanel;
---
+++ components/panel/Panel.tsx
@@ -0,0 +1,43 @@
+// React
+import { FC } from "react";
+
+// Components
+import Button from "components/ui/Button";
+
+export interface PanelProps {
+  title?: string;
+  className?: string;
+  actions?: PanelAction[];
+}
+
+export interface PanelAction {
+  text: string;
+  onClick: () => void;
+}
+
+export const Panel: FC<PanelProps> = ({
+  children,
+  title = "Title",
+  className = "",
+  actions = [],
+}) => {
+  return (
+    <section className="space-y-5">
+      <header className="flex items-center justify-between space-x-5">
+        <h2 className="text-xl font-bold select-none">{title}</h2>
+
+        <section className="space-x-2">
+          {actions.map(({ text, onClick }) => (
+            <Button key={text} onClick={() => onClick()}>
+              {text}
+            </Button>
+          ))}
+        </section>
+      </header>
+
+      <main className={className}>{children}</main>
+    </section>
+  );
+};
+
+export default Panel;
