9cb2cc5dfadb0077cf5f3485fddfefe8c2705a13
zhangxiangliang <admin@pushme.top>
2022-04-10 04:34:05 +0800

    feat: generate status in generate page

--- pages/generate.tsx
+++ pages/generate.tsx
@@ -4,15 +4,16 @@ import { NextPage } from "next";
 // Components
 import Panel from "components/panel/Panel";
 import Layout from "components/layout/Layout";
+import GenerateStatusPanel from "components/panel/GenerateStatusPanel";
 import GenerateMetadataPanel from "components/panel/GenerateMetadataPanel";
 import GenerateAttributesPanel from "components/panel/GenerateAttributesPanel";
 
 const Generate: NextPage = () => {
   return (
     <Layout>
-      <section className="flex w-full md:space-x-2 md:space-y-0 space-y-2 ">
+      <section className="flex w-full md:space-x-2 md:space-y-0 space-y-2 md:flex-nowrap flex-wrap">
         {/* Left */}
-        <section className="lg:w-3/4 md:w-2/3 sm:w-1/2 w-full">
+        <section className="lg:w-3/4 md:w-2/3  w-full">
           <section className="space-y-2">
             {/* Metadata */}
             <GenerateMetadataPanel />
@@ -23,14 +24,19 @@ const Generate: NextPage = () => {
         </section>
 
         {/* Right */}
-        <section className="lg:w-1/4 md:w-1/3 sm:w-1/2 w-full space-y-2">
-          {/* Preview */}
-          <Panel
-            title="预览"
-            className="bg-black bg-opacity-50 w-full pb-full h-0 rounded-md overflow-hidden"
-          >
-            <canvas />
-          </Panel>
+        <section className="lg:w-1/4 md:w-1/3 w-full space-y-2">
+          <section className="space-y-2 md:sticky md:top-0 relative">
+            {/* Preview */}
+            <Panel
+              title="预览"
+              className="bg-black bg-opacity-50 w-full rounded-md pb-full h-0"
+            >
+              <canvas />
+            </Panel>
+
+            {/* Status */}
+            <GenerateStatusPanel />
+          </section>
         </section>
       </section>
     </Layout>
